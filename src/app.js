c26da374555c279ba3a4b9b266fcfcfa52794aff
Oviedo Maltez <f.oviedo.maltez@accenture.com>
2019-07-21 22:46:43 -0500

    Completed the module

--- src/app.js
+++ src/app.js
@@ -8,20 +8,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
- 
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
- 
+
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
- 
+
 const users = JSON.parse(userData);
 
-app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts})); 
-app.get('/savings', (req, res) => {res.render('account', { account: accounts.savings})});
-app.get('/checking', (req, res) => {res.render('account', {account: accounts.checking})});
-app.get('/credit', (req, res) => {res.render('account', {account: accounts.credit})});
-app.get('/profile', (req, res) => {res.render('profile', {user: users[0]});});
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
+app.get('/savings', (req, res) => { res.render('account', { account: accounts.savings }) });
+app.get('/checking', (req, res) => { res.render('account', { account: accounts.checking }) });
+app.get('/credit', (req, res) => { res.render('account', { account: accounts.credit }) });
+app.get('/profile', (req, res) => { res.render('profile', { user: users[0] }); });
+app.get('/transfer', (req, res) => { res.render('transfer'); });
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - accounts.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.from].balance) + parseInt(accounts.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', accounts.json), 'utf8');
+    res.render('/transfer', { message: 'Transfer Completed' });
+});
+app.get('/payment', (req, res) => { res.render('payment', { account: accounts.credit }); });
+app.post('/payment', (req, resp) => {
+    accounts.credit.balance = accounts.credit.balance - req.body.amount;
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+    res.render('/payment', (req, res) => { 'payment', {message: "Payment Successful", account: accounts.credit} });
+});
 
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
